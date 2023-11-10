7337d3b4d7c4815fcf455b143f4ac4db4db86fa8
Siobhantt <luisacardozold@hotmail.com>
2023-11-10 08:56:44 +0100

    Mirar Binding

---
+++ IntentoBinding/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace IntentoBinding
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ IntentoBinding/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace IntentoBinding
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+
+            //Esta es la manera de bindear en codigo C#
+            //Tambien se puede hacer en el XAML
+            InitializeComponent();
+            //label.BindingContext = slider;
+            //label.SetBinding(Label.RotationProperty, "Value");
+            
+            //robot.BindingContext = slider;
+            //robot.SetBinding(Image.RotationProperty, "Value");
+                
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
